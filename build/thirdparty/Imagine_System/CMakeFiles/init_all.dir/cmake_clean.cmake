c3d8839f0922cac4b3dcdeda683a3c4866c3f918
ImagineJHY <imaginejhy@163.com>
2023-10-03 21:07:08 +0800

    add Makefile

---
+++ build/thirdparty/Imagine_System/CMakeFiles/init_all.dir/cmake_clean.cmake
@@ -0,0 +1,8 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/init_all"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/init_all.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- init.py
+++ init.py
@@ -2,36 +2,18 @@ import os
 import subprocess
 import yaml
 
-with open('thirdparty/Imagine_System/config/submodules.yaml', 'r') as file:
-    config_data = yaml.safe_load(file)
+# 确保Imagine System路径中所有的中间目录都存在
+thirdparty_dir_path = 'thirdparty/'
+os.makedirs(os.path.dirname(thirdparty_dir_path), exist_ok=True)
 
-# 获取所有子模块信息
-submodules_info = config_data['submodules']
+system_dir_path = os.path.join(thirdparty_dir_path, 'Imagine_System')
 
-# 确保路径中所有的中间目录都存在
-submodule_dir_path = 'thirdparty/Imagine_System/worker/'
-os.makedirs(os.path.dirname(submodule_dir_path), exist_ok=True)
-
-for submodule in submodules_info:
-    # 构造完整的子模块路径
-    submodule_path = os.path.join(submodule_dir_path, submodule['name'])
-
-    # 判断子模块目录是否存在
-    if os.path.isdir(submodule_path):
-        # 子模块已存在，切换到指定commit
-        checkout_command = ["git", "checkout", submodule['git_commit']]
-        process = subprocess.Popen(checkout_command, cwd=submodule_path)
-        process.wait()
-        # with tools.chdir(submodule['name']):
-        #     checkout_command = f"git checkout {submodule['git_commit']}"
-        #     subprocess.call(checkout_command, shell=True)
-    else:
-        # 子模块未存在，克隆并切换到指定commit
-        clone_command = f"git clone --branch {submodule['git_branch']} {submodule['git_url']} {submodule_path}"
-        subprocess.call(clone_command, shell=True)
-        checkout_command = ["git", "checkout", submodule['git_commit']]
-        process = subprocess.Popen(checkout_command, cwd=submodule_path)
-        process.wait()
-        # with tools.chdir(submodule['name']):
-        #     checkout_command = f"git checkout {submodule['git_commit']}"
-        #     subprocess.call(checkout_command, shell=True)
+# 判断Imagine System目录是否存在
+if os.path.isdir(system_dir_path):
+    # Imagine System存在直接退出
+    print("Imagine_System exist!")
+else:
+    #Imagine System不存在创建
+    submodule_command = ["git", "submodule", "add", "-f", "https://github.com/ImagineJHY/Imagine_System.git"]
+    process = subprocess.Popen(submodule_command, cwd = thirdparty_dir_path)
+    process.wait()
