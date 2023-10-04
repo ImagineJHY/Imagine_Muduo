94584cb67b43b5f518fdc66462568376e623705a
ImagineJHY <imaginejhy@163.com>
2023-10-05 00:24:49 +0800

    update .gitignore

--- build/thirdparty/Imagine_System/CMakeFiles/init_all.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/init_all"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/init_all.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/thirdparty/Imagine_System/CMakeFiles/prepare.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/prepare"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/prepare.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
