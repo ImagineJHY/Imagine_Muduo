22db315f85a666b0c3a598d7bc0b5d1441e84ec4
ImagineJHY <imaginejhy@163.com>
2023-10-05 13:37:59 +0800

    update url from ssh to http

--- init_bak.py
+++ init_bak.py
@@ -11,10 +11,9 @@ system_dir_path = os.path.join(thirdparty_dir_path, 'Imagine_System')
 # 判断Imagine System目录是否存在
 if os.path.isdir(system_dir_path):
     # Imagine System存在直接退出
-    print("[MAKE INIT]: Imagine_System exists.")
+    print("Imagine_System exist!")
 else:
     #Imagine System不存在创建
-    print("[MAKE INIT]: Imagine_System NOT exists, starting create...")
-    submodule_command = ["git", "submodule", "add", "-f", "https://github.com/ImagineJHY/Imagine_System.git"]
+    submodule_command = ["git", "submodule", "add", "-f", "git@github.com:ImagineJHY/Imagine_System.git"]
     process = subprocess.Popen(submodule_command, cwd = thirdparty_dir_path)
     process.wait()
