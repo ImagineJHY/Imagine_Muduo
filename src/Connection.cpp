7f75fc121ca6d8c8ecc2fd66db19f7be010cbafa
Imagine <imaginejhy@163.com>
2023-11-18 21:17:54 +0800

    update muduo

--- src/Connection.cpp
+++ src/Connection.cpp
@@ -117,6 +117,7 @@ void Connection::ProcessWrite()
 {
     write_callback_(this);
     write_buffer_.Write(channel_->Getfd());
+    LOG_INFO("write data %sb", write_buffer_.GetLen());
     if (clear_write_buffer_) {
         write_buffer_.Clear();
     }
